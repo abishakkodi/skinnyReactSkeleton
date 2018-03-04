ea218a22708ec7689befe2bd82d839b1bdd76571
Abishak Kodi <abishak.kodi@gmail.com>
2018-03-04 14:42:13 -0800

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+// import Home from './Home';
+// import DynamicPage from './DynamicPage';
+// import NoMatch from './NoMatch';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route exact path="/dynamic" component={DynamicPage} />
+//           <Route component={NoMatch} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// };
+
+
+const App = () => {
+
+  return (
+
+    <div>
+      <h1> Skinny React App </h1>
+      <h4> </h4>
+    </div>)
+}
+
+export default App;
